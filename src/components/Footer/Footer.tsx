fbc66f73a25c6250899f4b6d31ef5fa1f71c810e
Younghoo Nam <younghoonam@Younghooui-MacBookPro.local>
2026-02-23 00:37:13 +0900

    chore: update blog footer year to 2026

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.copywrite}>© 2025 Younghoo Nam</div>
+      <div className={styles.copywrite}>© 2026 Younghoo Nam</div>
       <div className={styles.links}>
         <a href="https://www.github.com/younghoonam" target="_blank" rel="noopener noreferrer">
           <img className={styles.icon} src="/icons/github-mark.svg" alt="GitHub" />
